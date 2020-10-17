fa267adfb61917617b222b00f5bbf0eedcda87fe
JohnattanIsrael <johnattanisraelangeles@gmail.com>
2020-10-16 19:59:03 -0500

    started react-redux project for glms

---
+++ src/components/app.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <h1>Gamified LMS  on React and MongoDB</h1>
+        <h2>Hello there</h2>
+      </div>
+    );
+  }
+}

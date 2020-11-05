da952fcff690b6b485e343b6878886a06f256cdd
JohnattanIsrael <johnattanisraelangeles@gmail.com>
2020-11-04 20:05:04 -0600

    added flex box and grid elements to login-wrapper

--- src/components/sign.js
+++ src/components/sign.js
@@ -16,87 +16,89 @@ const Sign = (props) => {
     } = props;
 
     return (
-        <section>
-            <div className='login'>
+        <section className='login-wrapper'>
+            <div className='login-content-wrapper'>
+                <div className='login'>
 
-                {hasAccount ? (
+                    {hasAccount ? (
 
-                    <div className='login-left'>
-                        <div className='login-left__title'>
-                            Don´t have an account ?
-                        </div>
-                        <div className='login-left__button'>
-                            <div onClick={() => setHasAccount(!hasAccount)}>
-                                <div className='login-left__legend'>
-                                    Sign up
-                                </div>
-                                <div className='button'>
-                                    <div className='button__green'></div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                ) : (
-
-                        <div>
+                        <div className='login-left'>
                             <div className='login-left__title'>
-                                Have an account ?
-                            </div>
+                                Don´t have an account ?
+                        </div>
                             <div className='login-left__button'>
                                 <div onClick={() => setHasAccount(!hasAccount)}>
                                     <div className='login-left__legend'>
-                                        Sign in
-                                    </div>
+                                        Sign up
+                                </div>
                                     <div className='button'>
-                                        <div className='button__red'></div>
+                                        <div className='button__green'></div>
                                     </div>
                                 </div>
                             </div>
                         </div>
 
-                    )}
-
-                <div className='login-right'>
-                    <div className='login-right__form'>
-                        <label>Username</label>
-                        <input
-                            type='text'
-                            autoFocus
-                            required
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                        />
-                        <p className='error-message'>{emailError}</p>
-                        <label>Password</label>
-                        <input
-                            type='password'
-                            required
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                        />
-                        <p className='error-message'>{passwordError}</p>
-                    </div>
-                    {hasAccount ? (
-                        <div className='login-right__button'>
-                            <button onClick={handleLogin}>
-                                <div className='loging-right__button-button button__blue' />
-                                <div className='login-right__legend'>
-                                    Sign In
-                                </div>
-                            </button>
-                        </div>
                     ) : (
-                            <div className='login-right__button'>
 
-                                <button onClick={handleSignUp}>
+                            <div>
+                                <div className='login-left__title'>
+                                    Have an account ?
+                            </div>
+                                <div className='login-left__button'>
+                                    <div onClick={() => setHasAccount(!hasAccount)}>
+                                        <div className='login-left__legend'>
+                                            Sign in
+                                    </div>
+                                        <div className='button'>
+                                            <div className='button__red'></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                        )}
+
+                    <div className='login-right'>
+                        <div className='login-right__form'>
+                            <label>Username</label>
+                            <input
+                                type='text'
+                                autoFocus
+                                required
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                            />
+                            <p className='error-message'>{emailError}</p>
+                            <label>Password</label>
+                            <input
+                                type='password'
+                                required
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
+                            <p className='error-message'>{passwordError}</p>
+                        </div>
+                        {hasAccount ? (
+                            <div className='login-right__button'>
+                                <button onClick={handleLogin}>
                                     <div className='loging-right__button-button button__blue' />
                                     <div className='login-right__legend'>
-                                        Sign Up
-                                    </div>
+                                        Sign In
+                                </div>
                                 </button>
                             </div>
-                        )}
+                        ) : (
+                                <div className='login-right__button'>
+
+                                    <button onClick={handleSignUp}>
+                                        <div className='loging-right__button-button button__blue' />
+                                        <div className='login-right__legend'>
+                                            Sign Up
+                                    </div>
+                                    </button>
+                                </div>
+                            )}
+                    </div>
                 </div>
             </div>
         </section>

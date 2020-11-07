1dcb50cc5bc262af4bec8e1fe9595f55f9318d57
JohnattanIsrael <johnattanisraelangeles@gmail.com>
2020-11-06 19:47:10 -0600

    refactored jsx of journeys and addes styles to the journey cards

--- src/components/journey/journeys.js
+++ src/components/journey/journeys.js
@@ -4,36 +4,36 @@ import { Link } from "react-router-dom";
 export default function (props) {
 
     const position = () => {
-        if((props.journey_id + 1) % 3 == 0) {
-            return('right')
+        if ((props.journey_id + 1) % 3 == 0) {
+            return ('right')
         } else if ((props.journey_id + 1) % 2 == 0) {
-            return('center')
+            return ('center')
         } else {
-            return('left')
+            return ('left')
         }
     }
 
     const image = () => {
         return (
-            // Math.floor(Math.random() * 12) + 1
-            1
+            Math.floor(Math.random() * 12) + 1
         )
     }
 
     return (
         <div className={`journey journey-position__${position()}`}>
-            <div className={`journey-wrapper journey-image__${image()}`}>
-                <p>background image</p>
-                <Link
-                    to={`/cards`}
-                >
-                            
-                    <h1 className='journey-wrapper__more-transparent'>{props.journey_name}</h1>
-                    <h1 className='journey-wrapper__transparent'>{props.journey_name}</h1>
-                    <h1 className='journey-wrapper__solid'>{props.journey_name}</h1>
-                
-                </Link>
-            </div>
+            <Link to={'/cards'}>
+                <div className={`journey-card journey-image__${image()}`}>
+                    <div className='journey-links'>
+
+                        <h1 className='journey-links__almost-gone'>{props.journey_name}</h1>
+                        <h1 className='journey-links__more-transparent'>{props.journey_name}</h1>
+                        <h1 className='journey-links__transparent'>{props.journey_name}</h1>
+                        <h1 className='journey-links__solid'>{props.journey_name}</h1>
+
+
+                    </div>
+                </div>
+            </Link>
 
         </div>
     )
